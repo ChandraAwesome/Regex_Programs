ca34e22208be8be862b495aa27c8321e215a05e9
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-25 21:40:03 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello, World!");
+
+            //LinkedLists obj= new LinkedLists();
+            //obj.AddNode(20);
+            //obj.AddNode(30);
+            //obj.AddNode(40);
+            //obj.PrintList();
+
+            //var dataa = new LinkedList<string>();
+
+            // Exceptionss.Errors();
+
+            Regexx result = new Regexx();
+
+           //Console.WriteLine( result.Checkimage("image1.png"));
+           // Console.WriteLine(result.Checkwebsite("www.example.com"));
+           // Console.WriteLine(result.Checkmatch("abbb"));
+           // Console.WriteLine(result.Checkunderscore("a_b_c"));
+            result.CheckHtmlTags("<p>The <code>Regex</code> is a compiled representation of a regular expression.</p>");
+
+        }
+    }
+}
